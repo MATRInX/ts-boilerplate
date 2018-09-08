1f6fc9fe67e32dbed7c71fbbe8c4efddbd749054
Marcin Wojtania <marcin.wojtania@gmail.com>
2018-09-08 13:39:11 +0200

    Fix duplicated todos name in state

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import Todo from '../models/Todo';
-//import { State } from '../reducers/todos';
-//import { State }from '../store/configureStore';
 import * as fromTodo from '../reducers/todos';
 import { toggleTodo, ToggleTodoAction } from '../actions/todos';
 
 export interface TodoListStoreProps {
-  //todos: Todo[]  
   todos: fromTodo.State
 }
 
@@ -36,7 +33,7 @@ constructor(props: TodoListProps){
       <div>
         {
           this.props.todos ? (
-            this.props.todos.todos.map((todoItem: Todo) => 
+            this.props.todos.map((todoItem: Todo) => 
             <div key={todoItem.id} onClick={() => this.onTodoClick(todoItem.id)}>
               {todoItem.name} <span>{todoItem.done ? 'is DONE' : 'has to be done!'}</span>
             </div>)
@@ -57,6 +54,5 @@ const mapDispatchToProps = (dispatch: Dispatch<ToggleTodoAction>, ownProps: Todo
   onTodoClick: (todoId: number) => dispatch(toggleTodo(todoId))
 });
 
-//export default connect<TodoListStoreProps, TodoListDispatchProps, TodoListProps, TodoListStoreProps>
 export default connect<TodoListStoreProps, TodoListDispatchProps, TodoListProps, any>
   (mapStateToProps, mapDispatchToProps)(TodoList);
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,27 +1,22 @@
 import Todo from '../models/Todo';
 import { ActionTypes, Action } from '../actions/todos';
 
-export interface State {
-  todos: Todo[]
-};
+export type State = Todo[];
 
-export const initialState: State = {
-  todos: []
-};
+export const initialState: State = [];
 
 export const reducer = (state: State = initialState, action: Action): State => {
   switch(action.type) {
     case ActionTypes.ADD_TODO:
-      return {
+      return [
         ...state, 
-        todos: [...state.todos, action.payload.todo]
-      };
+        action.payload.todo
+      ];
     case ActionTypes.TOGGLE_TODO:
       const { todoId } = action.payload;
-      return {
-        ...state,
-        todos: state.todos.map(todo => todo.id === todoId ? { ...todo, done: !todo.done} : todo)
-      };
+      return [
+        ...state.map(todo => todo.id === todoId ? { ...todo, done: !todo.done} : todo)
+      ];
     default:
       return state;
   }
